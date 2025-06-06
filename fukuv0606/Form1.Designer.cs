425ff5069952b44654bd42cbfd38630426aa8018
kishino <kisino0325@gmail.com>
2025-06-06 14:02:08 +0900

    fukuv0606

--- fukuv0606/Form1.Designer.cs
+++ fukuv0606/Form1.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(597, 315);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 100);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(352, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- fukuv0606/Form1.cs
+++ fukuv0606/Form1.cs
@@ -1,10 +1,38 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace fukuv0606
 {
     public partial class Form1 : Form
     {
+        int vx = 5;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = -vx;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "kisino";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            if (label1.Left <= 0) ;
+            {
+                vx = Math.Abs(vx);
+            }
+            else if(label1.Right>=ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
         }
     }
 }
